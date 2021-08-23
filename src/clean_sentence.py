75c7f801449d4aa8cc57c18759ccd9e64798d457
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-23 13:52:05 +0545

    generate similarity score of the two different sentences

--- src/clean_sentence.py
+++ src/clean_sentence.py
@@ -1,29 +1,28 @@
+
+from nltk.corpus import stopwords
 import re
 
-import nltk
-from nltk.corpus import stopwords
 
-
-def clean_sentence(sentence):
-
-    sentence = re.sub(r"\[![0-9]&)-^$#+*\]", " ", sentence)     
+def clean_sentence(sentence : str):
+    '''
+    '''
+    sentence = re.sub(r"\[![0-9]&)-^$#+*\]", " ", sentence) 
+    sentence = re.sub(r"\s+", " ", sentence)     
+    stop_words = set(stopwords.words("english"))
     get_words = sentence.lower().split()
-    stop_words= set(stopwords.words("english"))
     cleaned_sentence = list(set([word for word in get_words if word not in stop_words]))
     cleaned_sentence = [" ".join(cleaned_sentence[::])]
 
     return cleaned_sentence
 
-def clean_data(dataframe, sentence):
-
-    """
-        
-    """
-    cleaned_text = []
+def get_clean_dataframe(dataframe, sentence : str):
+    '''
+    '''
+    cleaned_sentence = []
     for index in range(len(dataframe[sentence])):
         uncleaned_text = dataframe[sentence][index]
-        cleaned_words = clean_sentence(uncleaned_text)
-        cleaned_text.append(cleaned_words[0])
-    dataframe['cleaned_title'] = cleaned_text
+        cleaned_text = clean_sentence(uncleaned_text)
+        cleaned_sentence.append(cleaned_text[0])
+    dataframe['cleaned_sentence'] = cleaned_sentence
 
     return dataframe
---
+++ src/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sentence_transformers import  util, SentenceTransformer
+
+def get_sentence_embeddings(model, sentence : list ) :
+    
+    '''
+    model = trained machine learning/ deep learning model
+    -----------------------------------------------------
+    convert sentence into embeddings
+    -----------------------------------------------------
+    return embeddings of given sentence
+    '''
+
+    return model.encode(sentence)
+
+def get_high_similarity_score(model, dataframe, sentance_1 : str, sentence_2 : str):
+
+    '''
+    machine learning/ deep learning model
+    --------------------------------------------------------------
+    generate embeddings by using  ml model and calculate 
+    similarity score of sentences to dataframe for that model
+    --------------------------------------------------------------
+    return Similarity Score of multiple model in one dictionary
+    '''
+
+    similarity_score = []
+    model = SentenceTransformer(model)
+    querry_embedding = get_sentence_embeddings(model, sentance_1)
+    sentence_embedding = get_sentence_embeddings(model, list(dataframe[sentence_2]))
+    for  index in range(len(dataframe[sentence_2])):
+        similarity_score.append(np.array(util.pytorch_cos_sim(querry_embedding, sentence_embedding[index])))                           
+          
+    return np.array(similarity_score).flatten()
+
+def get_high_similarity_score_index(similarity_score):
+    '''
+    take similarity_score as numpy array
+    --------------------------------------------------------------
+    sort the array and and calcuate their corresponding index
+    --------------------------------------------------------------
+    return highest 4 score index 
+    '''
+    indices = np.argsort(similarity_score)[::-1]
+    return indices[:4]
+
